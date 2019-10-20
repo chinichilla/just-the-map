2f4559f7d6fe19b293eb488ab780311fd9b2132f
Dan <djchinia@gmail.com>
2019-10-20 17:16:25 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Router, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import { createBrowserHistory as createHistory } from "history";
+import "./App.css";
+const history = createHistory();
+function App() {
+  return (
+    <div className="App">
+      <Router history={history}>
+        <Route path="/" exact component={HomePage} />
+      </Router>
+    </div>
+  );
+}
+export default App;
